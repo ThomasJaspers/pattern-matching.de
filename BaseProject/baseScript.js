d2aa5501db9dbc0f1fb67e6bcc9a65c6107b3768
Thomas Jaspers <tritom42@googlemail.com>
2012-02-09 21:46:11 +0100

    Some initial CSS styling for the front page.

---
+++ BaseProject/baseScript.js
@@ -0,0 +1,9 @@
+function setTextAreaActive(id)
+{
+    document.getElementById(id).style.background='#e5fff3'
+}
+
+function setTextAreaInactive(id)
+{
+    document.getElementById(id).style.background='white'
+}
---
+++ BaseProject/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>pattern-matching.de</title>
+        <link rel="stylesheet" type="text/css" href="baseStyle.css" />
+        <script type="text/javascript" src="baseScript.js"></script>
+    </head>
+    <body>
+        
+        <div class="headlineContainer">
+            <h3>Thank you for visiting ...</h3>
+            <h2>pattern-matching.de</h2>            
+        </div>
+        
+
+        
+        <div class="main">
+            <div class="textwrapper">
+            Your Pattern:        
+
+            <form>         
+                <textarea rows="3" cols="80" id="pattern" maxlength="256"  onfocus="setTextAreaActive('pattern');" onblur="setTextAreaInactive('pattern')"></textarea>
+            </form>
+        </div>
+
+            <?php
+            // put your code here
+            ?>
+        </div>
+    </body>
+</html>
